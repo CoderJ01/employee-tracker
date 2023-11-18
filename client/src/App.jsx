15d65e6c94b550e03578a6c45c5e8a392db45868
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-18 14:49:59 -0800

    import Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
+// CSS
+import './App.css';
+
+// components
+import Header from './components/Header/Header.component';
 
 function App() {
-
   return (
-    <>  
-    </>
+    <div className='App'>  
+      <Header></Header>
+    </div>
   )
 }
 
-export default App
+export default App;
