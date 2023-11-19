abc53b2ee8fbad92b14e082d68ef65b6e13e8c75
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-18 18:19:14 -0800

    import page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,11 @@
+// React
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
 // CSS
 import './App.css';
+
+// pages
+import Dashboard from './pages/Dashboard/Dashboard.page';
 
 // components
 import Header from './components/Header/Header.component';
@@ -7,7 +13,12 @@ import Header from './components/Header/Header.component';
 function App() {
   return (
     <div className='App'>  
-      <Header></Header>
+      <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
