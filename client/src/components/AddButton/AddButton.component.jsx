fc930ce7e387b413dd30d05bb1e730a23f01c9b1
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-18 20:40:13 -0800

    create separate component for add button

---
+++ client/src/components/AddButton/AddButton.component.jsx
@@ -0,0 +1,12 @@
+// CSS
+import './AddButton.style.css';
+
+function Add() {
+    return (
+        <div className='add'>
+            <i className='fa fa-plus-circle'></i>
+        </div>
+    );
+}
+
+export default Add;
