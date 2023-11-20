40ca8ff7b5d37ad55953aa569c870c51eeb6d8e1
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-19 18:00:43 -0800

    input validation for email and salary (file name change)

--- client/src/utils/inputValidation.js
+++ client/src/utils/inputValidation.js
@@ -2,3 +2,8 @@ export const isValidEmail = (email) => {
     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regex.test(String(email).toLowerCase());
 };
+
+export const isValidNumeric = (numeric) => {
+    const regex = /^\d+$/;
+    return regex.test(String(numeric).toLowerCase());
+};
