bf285628ba33eba0b2fd82b541c22888035ff396
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-19 17:48:43 -0800

    import util functions

--- client/src/components/Employees/Employees.component.jsx
+++ client/src/components/Employees/Employees.component.jsx
@@ -4,6 +4,9 @@ import { useState } from 'react';
 // CSS
 import './Employees.style.css';
 
+// util
+import { trackInput, processSubmission } from './Employees.util';
+
 // components
 import AddButton from '../AddButton/AddButton.component';
 
@@ -15,35 +18,12 @@ function Employees() {
     const [errorText, setErrorText] = useState('');
 
     function handleChange(e) {
-        if(e.target.name === 'lastname') {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-        if(e.target.name === 'firstname') {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-        if(e.target.name === 'email') {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-        if(e.target.name === 'role') {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-        if(e.target.name === 'department') {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-        if(e.target.name === 'salary') {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
+        trackInput(e, formState, setFormState);
     }
 
     function handleSubmit(e) {
         e.preventDefault();
-
-        if(lastname === '' || firstname === '' || email === '' || salary === '' || department === '' || salary === '') {
-            setErrorText('All inputs must be filled in');
-            return;
-        }
-
-        setErrorText('');
+        processSubmission(e, lastname, firstname, email, role, department, salary, setErrorText);
     }
 
     return (
