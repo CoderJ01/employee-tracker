a6db5a192d5f9a067a569ff68ef760f26c1ae544
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-20 18:44:56 -0800

    add useState varibales and button for registration form

--- client/src/components/Register/Register.component.jsx
+++ client/src/components/Register/Register.component.jsx
@@ -1,42 +1,70 @@
+// React
+import { useState } from 'react';
+
 // CSS
 import './Register.style.css';
 
+// util
+import { trackInput, processSubmission } from './Register.util';
+
 function Register() {
+    const [formState, setFormState] = useState({ lastname: '', firstname: '', email: '', phonenumber: '', password: '', companyname: '' });
+    const [errorText, setErrorText] = useState('');
+
+    const r = <text style={{ color: 'red'}}>*</text>
+
+    function handleChange(e) {
+        trackInput(e, formState, setFormState);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        processSubmission(e, formState, setErrorText);
+    }
+
     return (
         <div className='register'>
             <form>
                 <h2>Register</h2>
                 <br/>
                 <div>
-                    <label htmlFor='lastname'>Last Name:</label><br/>
-                    <input type='text' name='lastname'/>
+                    <label htmlFor='lastname'>Last Name:{r}</label><br/>
+                    <input type='text' name='lastname' defaultValue={formState.lastname} onChange={handleChange}/>
                 </div>
                 <br/>
                 <div>
-                    <label htmlFor='firstname'>First Name:</label><br/>
-                    <input type='text' name='firstname'/>
+                    <label htmlFor='firstname'>First Name:{r}</label><br/>
+                    <input type='text' name='firstname' defaultValue={formState.firstname} onChange={handleChange}/>
                 </div>
                 <br/>
                 <div>
-                    <label htmlFor='email'>Email:</label><br/>
-                    <input type='text' name='email'/>
+                    <label htmlFor='email'>Email:{r}</label><br/>
+                    <input type='text' name='email' defaultValue={formState.email} onChange={handleChange}/>
                 </div>
                 <br/>
                 <div>
                     <label htmlFor='phonenumber'>Phone Number:</label><br/>
-                    <input type='text' name='phonenumber'/>
+                    <input type='text' name='phonenumber' defaultValue={formState.phonenumber} onChange={handleChange}/>
                 </div>
                 <br/>
                 <div>
-                    <label htmlFor='password'>Password (8+ characters):</label><br/>
-                    <input type='password' name='password'/>
+                    <label htmlFor='password'>Password (8+ characters):{r}</label><br/>
+                    <input type='password' name='password' defaultValue={formState.password} onChange={handleChange}/>
                 </div>
                 <br/>
                 <div>
-                    <label htmlFor='companyname'>Company Name:</label><br/>
-                    <input type='text' name='companyname'/>
+                    <label htmlFor='companyname'>Company Name:{r}</label><br/>
+                    <input type='text' name='companyname' defaultValue={formState.companyname} onChange={handleChange}/>
                 </div>
                 <br/>
+                <span style={{ color: 'red', fontWeight: 'bold' }}>{errorText}</span>
+                {
+                    errorText === '' ? ('') :
+                    (<><br/><br/></>)
+                }
+                <div className='register-button'>
+                    <button onClick={handleSubmit}>Register</button>
+                </div>
                 <br/>
             </form>
         </div>
---
+++ client/src/components/Register/Register.util.js
@@ -0,0 +1,47 @@
+// util 
+import { isValidEmail } from "../../utils/inputValidation";
+import { postInfo } from "../../utils/requests";
+
+export function trackInput(e, formState, setFormState) {
+    if(e.target.name === 'lastname') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    if(e.target.name === 'firstname') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    if(e.target.name === 'email') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    if(e.target.name === 'phonenumber') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    if(e.target.name === 'password') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+    if(e.target.name === 'companyname') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+}
+
+export function processSubmission(e, form, setErrorText) {
+    e.preventDefault();
+    
+    if(form.lastname === '' || form.firstname === '' || form.email === '' || form.password === '' || form.companyname === '') {
+        setErrorText('All required (*) inputs must be filled in!');
+        return;
+    }
+
+    if(!isValidEmail(form.email)) {
+        setErrorText('Invalid email!');
+        return;
+    }
+
+    if(form.password.length < 8) {
+        setErrorText('Password must be at least 8 characters!');
+        return;
+    }
+
+    setErrorText('');
+
+    postInfo('', form, '');
+}
