71887abc3923da83f554b7600669b4cc5f978324
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-20 21:40:05 -0800

    import and implement util function

--- client/src/components/Header/Header.component.jsx
+++ client/src/components/Header/Header.component.jsx
@@ -1,12 +1,20 @@
 // CSS 
 import './Header.style.css';
 
+// util
+import removeCookie from './Header.util';
+
 function Header() {
+    function handleLogout(e) {
+        e.preventDefault();
+        removeCookie();
+    }
+
     return(
         <header>
             <text className='header-blank'>Logout</text>
             <h1>Employee Tracker</h1>
-            <text className='header-logout'>Logout</text>
+            <text className='header-logout' onClick={handleLogout}>Logout</text>
         </header>
     );
 }
