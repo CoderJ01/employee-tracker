66f387d1d64180cdc490ea7999a03526dfbadcca
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-25 13:05:15 -0800

    create controller for employer

---
+++ server/src/main/java/com/employeetracker/server/controller/EmployerController.java
@@ -0,0 +1,71 @@
+package com.employeetracker.server.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.employeetracker.server.exception.ResourceNotFoundException;
+import com.employeetracker.server.model.Employer;
+import com.employeetracker.server.repository.EmployerRepository;
+
+@CrossOrigin(origins = "http://localhost:5432")
+@RestController
+@RequestMapping("/api/v1")
+public class EmployerController {
+    @Autowired
+    private  EmployerRepository employerRepository;
+
+    // get all employers
+    @GetMapping("/employers")
+    public List<Employer> getAllEmployers() {
+        return employerRepository.findAll();
+    }
+
+    // create employer REST API
+    @PostMapping("/employees")
+    public Employer createEmployer(@RequestBody Employer employer) {
+        return employerRepository.save(employer);
+    }
+
+    // get employer by id REST api
+    @GetMapping("/employers/{id}")
+    public ResponseEntity<Employer> getEmployerById(@PathVariable Long id) {
+        Employer employer = employerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employer with " + " id " + id + " does not exist"));
+        return ResponseEntity.ok(employer);
+    }
+
+    // update employer REST API
+    @PutMapping("/employers/{id}")
+    public ResponseEntity<Employer> updateEmployer(@PathVariable Long id, @RequestBody Employer employerDetails) {
+        Employer employer = employerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employer with " + " id " + id + " does not exist"));
+
+        employer.setFirstName(employerDetails.getFirstName());
+        employer.setLastName(employerDetails.getLastName());
+        employer.setEmail(employerDetails.getEmail());
+        employer.setPhoneNumber(employerDetails.getPhoneNumber());
+        employer.setCompanyName(employerDetails.getCompanyName());
+        employer.setRole(employerDetails.getRole());
+        employer.setPassword(employerDetails.getPassword());
+
+        Employer updatedEmployer = employerRepository.save(employer);
+        return ResponseEntity.ok(updatedEmployer);
+    }
+
+    // delete employee rest api
+    @DeleteMapping("/employers/{id}")
+    public ResponseEntity<Map<String, Boolean>> deleteEmployer(@PathVariable Long id){
+        Employer employer = employerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employer with " + " id " + id + " does not exist"));
+
+        employerRepository.delete(employer);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return ResponseEntity.ok(response);
+    }
+}
