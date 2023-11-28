2ee1a19f408d243b2bdd3d869a46a2e2e31772fb
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-27 18:41:36 -0800

    create method to check for duplicate names

--- server/src/main/java/com/employeetracker/server/generate/CreateUsername.java
+++ server/src/main/java/com/employeetracker/server/generate/CreateUsername.java
@@ -1,5 +1,11 @@
 package com.employeetracker.server.generate;
 
+import com.employeetracker.server.model.Employee;
+import com.employeetracker.server.model.Employer;
+
+import java.util.List;
+import java.util.Set;
+
 public class CreateUsername {
     public static String setUsername(String firstname, String lastname, int number) {
         String zeros = setNumberOfZeros(number);
@@ -18,6 +24,17 @@ public class CreateUsername {
         if (count == 1) return "00";
         if (count == 2) return "0";
         return "";
+    }
+
+    public static int checkDuplicateNames(List<Employer> databaseList, Employer request) {
+        int count = 1;
+        for(int i = 0; i < databaseList.size(); i++) {
+            if(request.getFirstName().equals(databaseList.get(i).getFirstName()) &&
+                    request.getLastName().equals(databaseList.get(i).getLastName())) {
+                count++;
+            }
+        }
+        return count;
     }
 }
 
