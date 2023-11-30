90025f13517394924168b0b48b6d52fa0dbaf69b
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-30 15:27:03 -0800

    clean

--- server/src/main/java/com/employeetracker/server/controller/EmployerController.java
+++ server/src/main/java/com/employeetracker/server/controller/EmployerController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import com.employeetracker.server.exception.ResourceNotFoundException;
