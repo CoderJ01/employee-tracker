90cc7bf2c18fd2347aa0524ee694c80f8a5615ac
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-30 17:22:14 -0800

    fetch user in App.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,10 +12,18 @@ import Header from './components/Header/Header.component';
 import Register from './components/Register/Register.component';
 import Login from './components/Login/Login.component';
 
+// util
+import { useFetch } from './utils/requests';
+
 function App() {
+  const data  = useFetch(
+    'employers',
+    true
+  );
+
   return (
     <div className='App'>  
-      <Header/>
+      <Header user={data}/>
       <BrowserRouter>
         <Routes>
           <Route path='/dashboard' element={<Dashboard/>}/>
