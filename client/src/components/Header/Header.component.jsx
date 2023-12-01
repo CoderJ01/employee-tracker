9629e4879dd87aace7de68bf4ff8dd6ad99ce9c4
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-30 17:37:05 -0800

    conditional header rendering

--- client/src/components/Header/Header.component.jsx
+++ client/src/components/Header/Header.component.jsx
@@ -4,7 +4,7 @@ import './Header.style.css';
 // util
 import removeCookie from './Header.util';
 
-function Header() {
+function Header({ user }) {
     function handleLogout(e) {
         e.preventDefault();
         removeCookie();
@@ -12,9 +12,23 @@ function Header() {
 
     return(
         <header>
-            <text className='header-blank'>Logout</text>
-            <h1>Employee Tracker</h1>
-            <text className='header-logout' onClick={handleLogout}>Logout</text>
+            {
+                user.length === 0 ? 
+                (
+                    <>
+                    <text className='header-blank'>Login</text>
+                    <h1>Employee Tracker</h1>
+                    <a href={`${import.meta.env.VITE_CLIENT_URL}/login`}className='header-logout'>Login</a>
+                    </>
+                ) : 
+                (
+                    <>
+                    <text className='header-blank'>Logout</text>
+                    <h1>Employee Tracker</h1>
+                    <text className='header-logout' onClick={handleLogout}>Logout</text>
+                    </>
+                )
+            }
         </header>
     );
 }
