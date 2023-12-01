c7eab13aad0ddea5ba179416d4b47627d670ad48
CoderJ01 <buildingguy@sbcglobal.net>
2023-11-30 18:54:21 -0800

    create AccessDenied component

---
+++ client/src/components/AccessDenied/AccessDenied.component.jsx
@@ -0,0 +1,12 @@
+// CSS
+import './AccessDenied.style.css';
+
+function AccessDenied() {
+    return (
+        <div className='access-denied'>
+            <text> Access denied! Sign up or log in for access to this page!</text>
+        </div>
+    );
+}
+
+export default AccessDenied;
