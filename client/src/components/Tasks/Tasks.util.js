586514d09f29e8f811576771857ce80ef1bfa10a
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-02 18:07:27 -0800

    edit task form functionality

--- client/src/components/Tasks/Tasks.util.js
+++ client/src/components/Tasks/Tasks.util.js
@@ -7,19 +7,27 @@ export function trackInput(e, formState, setFormState) {
     if(e.target.name === 'description') {
         setFormState({ ...formState, [e.target.name]: e.target.value });
     }
+    if(e.target.name === 'email') {
+        setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
 }
 
-export function processSubmission(e, form, setErrorText) {
+export function processSubmission(e, form, setErrorText, user) {
     e.preventDefault();
     
     if(form.title === '' || form.description === '') {
-        setErrorText('All inputs must be filled in!');
+        setErrorText('Title and description must be filled in!');
+        return;
+    }
+
+    if(form.email === '') {
+        setErrorText('Email must be selected! By default, no email is selected (even if it looks like one is).');
         return;
     }
     
     setErrorText('');
 
-    postInfo('', form, '');
+    postInfo(`tasks/${user.id}/${form.email}`);
 }
 
 export function deleteTask(id) {
