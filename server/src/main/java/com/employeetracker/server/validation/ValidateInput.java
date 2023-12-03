53f62787b9f4e1307975f57620e59b7249c6bbb0
CoderJ01 <buildingguy@sbcglobal.net>
2023-12-02 18:49:58 -0800

    create util model for employee selection

--- server/src/main/java/com/employeetracker/server/validation/ValidateInput.java
+++ server/src/main/java/com/employeetracker/server/validation/ValidateInput.java
@@ -1,9 +1,11 @@
 package com.employeetracker.server.validation;
 
+import com.employeetracker.server.model.Employee;
 import com.employeetracker.server.model.Employer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.List;
+import java.util.Set;
 
 public class ValidateInput {
     public static Employer validateUsernameOrEmail(List<Employer> allEmployers, Employer requested) {
@@ -21,5 +23,15 @@ public class ValidateInput {
         boolean passwordIsValid = bCryptPasswordEncoder.matches(passwordInput, databasePassword);
         if(passwordIsValid) return true;
         throw new RuntimeException("Wrong password!");
+    }
+
+    public static Employee selectEmployeeForTask(Set<Employee> employees, String email) {
+        Employee[] SQLlist = employees.toArray(new Employee[employees.size()]);
+        for(int i = 0; i < employees.size(); i++) {
+            if(email.equals(SQLlist[i].getEmail())) {
+                return SQLlist[i];
+            }
+        }
+        throw new RuntimeException("Employee does not exist!");
     }
 }
